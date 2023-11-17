10a9f636ba2299f29fbeffde30aceb3662e26cd0
quit <Matt@Matthews-MacBook-Pro.local>
2023-11-17 20:18:38 +0000

    Worked on making JavaScript valdation code more concise.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('button').addEventListener('click', () => {
-    // const form = document.querySelector('form')
-    //
-    // const items = Object.entries(form)
-    //
-    // items.forEach(([key, item]) => {
-    //   const isValid = item.validity.valid
-    //   const value = item.value
-    //
-    //   if (isValid) {
-    //     console.log(key, value)
-    //   }
-    // })
+    const form = document.querySelector('form')
+    const items = Object.entries(form)
+    const invalidItems = items.filter(item => !item[1].validity.valid);
 
-    // const formData = new FormData(form)
-    //
-    // formData.forEach((value, key) => {
-    //   console.log(key, value)
-    // })
-    // Object.entries(form)[0][1].validity.valid
-    // items = Object.entries(document.querySelector('form'))
-    // items.map(item => typeof item[1] === 'object')
-    // items.map(item => item[1].localName === 'input')
-    // items.map(item => item[1].validity.valid)
+    invalidItems.forEach(item => {
+      console.log(item[1].validationMessage)
+    })
+
   })
 
   document.querySelector('form').addEventListener('submit', (e) => {
